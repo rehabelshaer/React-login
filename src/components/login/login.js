0c5fba46032fb789562e7572d40c150f49eccec3
mohamed adel <mohamed.adel@appssquare.com>
2021-10-05 18:23:23 +0200

    login page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,94 +1,105 @@
-import React , { Component } from 'react';
-import "./style.css"
+import React, { Component } from "react";
+import "./style.css";
 // import { Link } from 'react-router-dom';
 import login from "../images/login.jpg";
 // import { Route, useHistory } from 'react-router';
 
-const initialState ={
-  username:"",
-  password:"",
-  nameError :"",
-  passwordError :"",
-}
-class Login extends Component{ 
- 
- state = initialState;
-
- handleChange = (e) => {
-  this.setState({
-    [e.target.name]: e.target.value,
-    });
+const initialState = {
+  username: "",
+  password: "",
+  nameError: "",
+  passwordError: "",
 };
+class Login extends Component {
+  state = initialState;
 
-validate =()=>{
- let nameError ="";
- let passwordError ="";
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-const user = {
-   username: this.state.username,
-   password: this.state.password,
- };
+  validate = () => {
+    if (this.state.username && this.state.password) {
+      return {
+        ...this.state,
+        nameError: "",
+        passwordError: "",
+      };
+    } else {
+      if (!this.state.username) {
+        this.setState((old) => ({
+          ...old,
+          nameError: "name can't be empty",
+        }));
+      }
+      if (!this.state.password) {
+        this.setState((old) => ({
+          ...old,
+          passwordError: "Enter Password",
+        }));
+      }
+      return false;
+    }
+  };
 
- if(!this.state.username){
-   nameError = "name can't be empty";
- }
- if (!this.state.password) {
-  passwordError = 'Enter Password';
-}
-}
+  handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+    const isVaild = this.validate();
+    if (isVaild) {
+      this.props.history.push("/home");
+    }
+  };
 
-handleSubmit = (e) => {
-  e.preventDefault(); 
-  const isVaild = this.validate();
-  if (isVaild){
-    this.setState( initialState, () => this.props.history.push('/home'));
+  render() {
+    return (
+      <div className="base-container">
+        <div className="content">
+          <div className="image">
+            <img src={login} />
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form">
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="username"
+                  onChange={this.handleChange}
+                  value={this.state.username}
+                />
+              </div>
+              <div className="error">{this.state.nameError}</div>
+
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  onChange={this.handleChange}
+                  value={this.state.password}
+                />
+              </div>
+              <div className="error">{this.state.passwordError}</div>
+
+              <div className="button">
+                <button type="submit" value="submit">
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
-
- render (){
-  return (
-    <div className="base-container" >
-      <div className="content">
-       <div className="image"> 
-            <img  src={login} />
-         </div> 
-  <form onSubmit={this.handleSubmit}>
-  <div className="form">
-    <div className="form-group">
-      <label htmlFor="username">Username</label>
-      <input 
-      type="text" 
-      name="username" 
-      placeholder="username" 
-      onChange={this.handleChange} 
-      value={this.state.username} />
-    </div>
-    <div className='error'>{this.state.nameError}</div>
-
-    <div className="form-group">
-      <label htmlFor="password">Password</label>
-      <input 
-      type="password" 
-      name="password" 
-      placeholder="password" 
-      onChange={this.handleChange} 
-      value={this.state.password}/>
-    </div>
-    <div className='error'>{this.state.passwordError}</div>
-
-    <div className="button">
-     <button type="submit" value="submit">
-       Login
-     </button>
-     </div>
-    </div>
-    </form>
-    </div>
-  </div>
-  )
-}
- }
 export default Login;
- // <Link to="/home">Login</Link> 
-{/* <Link to ="/">Login<Link>
-<Link to ="/home">Home<link></Link>  */}
+// <Link to="/home">Login</Link>
+{
+  /* <Link to ="/">Login<Link>
+<Link to ="/home">Home<link></Link>  */
+}
